c6158fcba15704aae18944e55e1215edcdc4e85c
Isa <lajansa@hotmail.com>
2018-02-07 00:04:41 +0800

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,13 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+  if (cardOne === cardTwo) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again");
+  }
+}
